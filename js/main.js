8327685be4e56830c8e4ae6870ca754384d9320f
minnug <parkminnug@gmail.com>
2024-02-18 00:16:57 +0900

    ✨ Implement global badge(popup)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 검색창 포커 시 input 넓이 가변
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -14,3 +15,24 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+// 스크롤 시 badge(팝업 이미지) 사라짐
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY > 500);
+  if (window.scrollY) {
+    // 배지 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  }  else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
